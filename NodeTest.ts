a45a37136dbd90a6906e6947a3f40d534c63d6f9
Jacqueline <Jacqueline@LAPTOP-BTCIPF43.hfu-open.fu.hs-furtwangen.de>
2017-06-23 11:34:32 +0200

    bearbeitung

--- NodeTest.ts
+++ NodeTest.ts
@@ -30,11 +30,16 @@ function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerRes
     _response.setHeader("Access-Control-Allow-Origin", "*");
     _response.setHeader("content-type", "text/html; charset=utf-8");
 
-    for (key in query)
-        _response.write(key + ":" + query[key]);
+    for (key in query) {
 
+        if (query[key] == "0") {
+            continue;
+        }
+        _response.write(key + ":" + query[key]);
+    }
     //    _response.setHeader("Access-Control-Allow-Origin", "*");
     //    _response.setHeader("content-type", "text/html; charset=utf-8");
     //    _response.write("Ich höre Stimmen!");
+
     _response.end();
 }
